fbe39cdc4b4f0d67e8629b187ff7bafac874be54
MichelleWomberk <michelle.womberk@ucb.edu.bo>
2024-08-16 10:36:18 -0400

    py

---
+++ Python/#hola mundo.py
@@ -0,0 +1,15 @@
+#hola mundo
+def HolaMundo():
+    print("hola mundo")
+HolaMundo()
+#obtener edad
+def ObtenerEdad():
+    obedad= input("ingrese su edad")
+    print("su edad es:",obedad)
+ObtenerEdad()
+#calcular edad
+def CalcularEdad():
+    anio= int(input ("ingrese su año de nacimiento"))
+    edadcalc= 2024- anio
+    print("su edad eeda", edadcalc)
+CalcularEdad()

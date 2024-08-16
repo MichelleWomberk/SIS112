b02bf6b8710f8bf6c7e744742b137a2ba4622041
MichelleWomberk <michelle.womberk@ucb.edu.bo>
2024-08-16 10:11:20 -0400

    js

--- mi primer programa en C#/Program.cs
+++ mi primer programa en C#/Program.cs
@@ -1,36 +1,14 @@
-﻿using System.Net.Security;
+﻿<!DOCTYPE html>
+<html>
+<head>
+<title>MICHELLE WEB</title>
 
-namespace mi_primer_programa_en_C_;
+</head>
+<body>
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        //primer metodo, mostrar un Hola mundo
-        HelloWorld();
-
-        // segundo metodo, mostrar la edad ingresada por el usuario
-        Console.WriteLine("introduzca su edad");
-        string? edadInput = Console.ReadLine();
-        if (string.IsNullOrEmpty(edadInput))
-        {
-            Console.WriteLine("introduzca in valor");
-        }
-        else
-        {
-            int edadInteger = Int32.Parse(edadInput);
-            ObtenerEdad(edadInteger);
-        }
-    }
-
-    public static void HelloWorld()
-    {
-        Console.WriteLine("Hello, World");
-    }
-
-    public static void ObtenerEdad(int edad)
-    {
-        Console.WriteLine("Tu edad es:" + edad);
-    }
-
-}
+<h1>mi primera pagina web</h1>
+<p>UCB</p>
+<button type= "button" click= "HolaMundo()">
+Click Me HOLA MUNDO!</button>
+</body>
+</html>

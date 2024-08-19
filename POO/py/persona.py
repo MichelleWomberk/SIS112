b52bd28a04911f5b05ffb46884892ecf25339822
MichelleWomberk <michelle.womberk@ucb.edu.bo>
2024-08-19 12:00:47 -0400

    POO python

---
+++ POO/py/persona.py
@@ -0,0 +1,20 @@
+class Persona:
+    def __init__(self,nombre,edad,carrera):
+        self.nombre = nombre
+        self.edad = edad
+        self.carrera = carrera
+    def saludar(self):
+        print(f"hola, mi nombre es{self.nombre}y tengo{self.edad}años")
+    def cumpleaños(self):
+        self.edad += 1 
+        print(f"!Feliz cumpleaños! Ahora tengo{self.edad}años")
+    def estudiar(self):
+        print(f"Estoy estudiando{self.carrera}")
+        
+    #crear una instancia de la clase persona
+persona1 = Persona("Juan",29,"medicina")
+    
+    
+persona1.saludar()
+persona1.carrera()
+        

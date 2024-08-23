09483c65b310c5f82b91d6d21c9496f32014d633
MichelleWomberk <michelle.womberk@ucb.edu.bo>
2024-08-23 11:00:24 -0400

    Update persona.py

--- POO/py/persona.py
+++ POO/py/persona.py
@@ -1,10 +1,11 @@
 class Persona:
+    #method -> constructor
     def __init__(self,nombre,edad,carrera):
         self.nombre = nombre
         self.edad = edad
         self.carrera = carrera
     def saludar(self):
-        print(f"hola, mi nombre es{self.nombre}y tengo{self.edad}años")
+        print(f"hola, mi nombre es {self.nombre} y tengo{self.edad}años")
     def cumpleaños(self):
         self.edad += 1 
         print(f"!Feliz cumpleaños! Ahora tengo{self.edad}años")
@@ -13,8 +14,9 @@ class Persona:
         
     #crear una instancia de la clase persona
 persona1 = Persona("Juan",29,"medicina")
-    
+persona2= Persona("Maria",21,"Odontologia")   
     
 persona1.saludar()
+persona2.saludar()
 persona1.carrera()
         

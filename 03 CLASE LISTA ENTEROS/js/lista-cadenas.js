dedf77aa1f02d48407e8f91cb6f95f1a1da25fc6
MichelleWomberk <michelle.womberk@ucb.edu.bo>
2024-10-07 12:18:58 -0400

    CLASE

--- 03 CLASE LISTA ENTEROS/js/lista-cadenas.js
+++ 03 CLASE LISTA ENTEROS/js/lista-cadenas.js
@@ -19,12 +19,105 @@ class ListaCadenas {
     }
 
     ordenar() {
-        this.lista.sort((a, b) => a - b); // Orden ascendente
+        this.lista.sort();
     }
 }
 
 let miLista = new ListaCadenas();
-miLista.agreagar('hola');
-miLista.agregar('mundo');
-console.log(miLista.lista); //Imprime
+miLista.agregar('hola')
+miLista.agregar('mundo')
+miLista.agregar('UCB')
+mostrarResultado();
 
+//RETORNAR LAS CADENAS CON MAYOR CARACTERES
+//var= let
+
+var a = miLista.lista[0]
+console.log(a);
+var aCant = a.length;
+console.log(aCant); 
+
+var b = miLista.lista[1]
+console.log(b);
+var bCant = b.length;
+console.log(bCant); 
+
+var c = miLista.lista[2]
+console.log(c);
+var cCant = c.length;
+console.log(cCant); 
+
+//buscar el mayor
+var mayor = 0
+var posicion = -1;
+if (aCant>mayor){
+    mayor= aCant;
+    posicion = 0;
+}
+
+if (bCant>mayor){
+    mayor= bCant;
+    posicion = 1; 
+}
+
+if (cCant>mayor){
+    mayor=cCant;
+    posicion = 2;
+}
+
+console.log('La mayor cantidad de caracteres tiene'+ mayor);
+console.log('La CADENA con mayor cantidad de caracteres es: '+ miLista.lista[posicion]);
+
+//for
+var mayor = 0;
+var posicion = -1;
+
+// Recorremos la lista
+for (var i = 0; i < miLista.lista.length; i++) {
+    var cadena = miLista.lista[i];
+    console.log(cadena);
+    
+    var cant = cadena.length;
+    console.log(cant);
+
+    // Comprobamos si la longitud actual es mayor que el valor anterior
+    if (cant > mayor) {
+        mayor = cant;
+        posicion = i;
+    }
+}
+
+console.log('La mayor cantidad de caracteres tiene: ' + mayor);
+console.log('La CADENA con mayor cantidad de caracteres es: ' + miLista.lista[posicion]);
+
+console.log(miLista.lista);
+
+
+function agregar() {
+    const valor = document.getElementById('inputValor').value;
+    miLista.agregar(valor);
+    document.getElementById('inputValor').value = '';
+    mostrarResultado();
+}
+
+function eliminar() {
+    const valor = document.getElementById('inputValor').value;
+    miLista.eliminar(valor);
+    document.getElementById('inputValor').value = '';
+    mostrarResultado();
+}
+
+function buscar() {
+    const valor = document.getElementById('inputValor').value;
+    const posicion = miLista.buscar(valor);
+    document.getElementById('resultado').innerHTML = `Posición de "${valor}": ${posicion}`;
+}
+
+function ordenar() {
+    miLista.ordenar();
+    mostrarResultado();
+}
+
+function mostrarResultado() {
+    document.getElementById('resultado').innerHTML = 'Lista actual: ' + miLista.lista.join(', ');
+}
--- 03 CLASE LISTA ENTEROS/js/lista-enteros.js
+++ 03 CLASE LISTA ENTEROS/js/lista-enteros.js
@@ -23,16 +23,78 @@ class ListaEnteros {
     }
 }
 
-let miLista = new ListaEnteros();
+const listaEnteros = new ListaEnteros();
+listaEnteros.agregar(21);
+listaEnteros.agregar(12);
+listaEnteros.agregar(15);
+listaEnteros.agregar(30);
+console.log(listaEnteros.lista);
 
-function actualizarResultado() {
-    document.getElementById('result').innerHTML = `Resultado: [ ${miLista.lista.join(', ')} ]`;
+var a = listaEnteros.lista[0];
+console.log(a);
+
+var b = listaEnteros.lista[1];
+console.log(b);
+
+var c = listaEnteros.lista[2];
+console.log(c);
+
+var d = listaEnteros.lista[3];
+console.log(d);
+
+var datoMenor = listaEnteros.lista[0]; // Inicializamos datoMenor con el primer elemento
+
+for (var i = 1; i < listaEnteros.lista.length; i++) {
+    var a = listaEnteros.lista[i];
+    if (a < datoMenor) { // Comparamos el elemento actual con datoMenor
+        datoMenor = a; // Actualizamos datoMenor si encontramos un menor
+    }
+    console.log("Dato Menor: " + datoMenor); // Mostramos el dato menor en cada iteración
 }
+var datoMayor = listaEnteros.lista[0]; // Inicializamos datoMayor con el primer elemento
+
+for (var i = 1; i < listaEnteros.lista.length; i++) {
+    var a = listaEnteros.lista[i];
+    if (a > datoMayor) { // Comparamos el elemento actual con datoMayor
+        datoMayor = a; // Actualizamos datoMayor si encontramos un mayor
+    }
+}
+
+console.log("Dato Mayor: " + datoMayor); // Mostramos el dato mayor al final
+
+/*
+//retornar el elemento menor 
+var detonar =-1
+//-------------------------------------------------------------------------------------------------------------------
+var a = ListaEnteros.lista[0];
+if (a > datoMenor){
+    datoMenor= a 
+}
+console.log ("dato Menor :"+ datoMenor);
+//------------------------------------------------------------------------------------------------------------------
+var b = ListaEnteros.lista[0];
+if (a > b){
+    datoMenor= b;
+}
+console.log ("dato Menor :"+ datoMenor);
+//--------------------------------------------------------------------------------------
+var c = ListaEnteros.lista[0];
+if (b > c){
+    datoMenor= c;
+}
+console.log ("dato Menor :"+ datoMenor);
+//---------------------------------------------------------------------------------
+var d = ListaEnteros.lista[0];
+if (c > d){
+    datoMenor= d;
+}
+console.log ("dato Menor :"+ datoMenor);
+*/
 
 function agregar() {
     const valor = parseInt(document.getElementById('inputValor').value);
     if (!isNaN(valor)) {
-        miLista.agregar(valor);
+        listaEnteros.agregar(valor);
         actualizarResultado();
         document.getElementById('inputValor').value = ''; // Limpiar input
     }
@@ -41,7 +103,7 @@ function agregar() {
 function eliminar() {
     const valor = parseInt(document.getElementById('inputValor').value);
     if (!isNaN(valor)) {
-        miLista.eliminar(valor);
+        listaEnteros.eliminar(valor);
         actualizarResultado();
         document.getElementById('inputValor').value = ''; // Limpiar input
     }
@@ -50,12 +112,12 @@ function eliminar() {
 function buscar() {
     const valor = parseInt(document.getElementById('inputValor').value);
     if (!isNaN(valor)) {
-        const index = miLista.buscar(valor);
+        const index = listaEnteros.buscar(valor);
         alert(index !== -1 ? `El número ${valor} está en la posición ${index}` : `${valor} no se encuentra en la lista.`);
     }
 }
 
 function ordenar() {
-    miLista.ordenar();
+    listaEnteros.ordenar();
     actualizarResultado();
 }
